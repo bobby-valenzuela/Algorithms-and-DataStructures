dded49f0afae8a4fe3db6d38f657ab796eb0c384
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2022-05-28 08:52:32 -0700

    Added capitalizeWords recursive function

--- 04_recursionExersices.js
+++ 04_recursionExersices.js
@@ -255,3 +255,36 @@ function someRecursive(ar, cb){
      
     
   }
+
+
+
+///// Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
+
+// Ugly initial working solution
+
+function capitalizeFirst (arr) {
+  // add whatever parameters you deem necessary - good luck!
+    
+    if ( arr.length === 0 ) return [];
+    const current = capitalizeFirst( arr.slice(0,-1) ) 
+    // ^ (above) Using slice(0,-1) as I want to return the array in the same order as before, but I can't make my changes to the returned array before the recursive call, I need to return the arrau first
+    let currentItem = arr[ arr.length - 1 ]
+    currentItem = `${currentItem[0].toUpperCase()}${currentItem.slice(1)}`
+    current.push( currentItem )    
+    return current;
+}
+
+// capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+
+// Better Solution
+
+function capitalizeWords (array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length-1)[0].toUpperCase());
+  return res;
+ 
+}
