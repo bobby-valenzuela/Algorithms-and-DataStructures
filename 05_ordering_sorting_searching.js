d9646f0a4d278a3457d653ce22e19304d47fa683
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2022-08-13 18:03:38 -0700

    Update and rename 05_ordering_sorting.js to 05_ordering_sorting_searching.js

--- 05_ordering_sorting_searching.js
+++ 05_ordering_sorting_searching.js
@@ -94,3 +94,25 @@ function validAnagram(first, second){
     return true;
     
 }
+
+// Linear search
+
+function binarySearch( arr, elem ){
+
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor( ( start + end ) / 2 )
+    
+    while ( arr[middle] != elem && start <= end ){
+        
+        if ( elem < arr[middle] ) end = middle - 1 
+        if ( elem > arr[middle] ) start = middle + 1 
+        middle = Math.floor( ( start + end ) / 2 )
+       
+    }
+    
+    return ( arr[middle] === elem ) ? middle : -1
+  
+}
+
+binarySearch([0,1,2,3,4,5,6,7,8,9,10,11],7)
