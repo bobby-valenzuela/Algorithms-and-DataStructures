9e1206d3b02cb2adf1ca41223e055174bcc9d0db
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-02-11 12:30:29 -0800

    MergeSortedArrays script

---
+++ 05_sorting_intermediate_mergeSort.js
@@ -0,0 +1,39 @@
+// Merges two sorted arrays
+function mergeSort(a,b){
+
+    const result = []
+    let i = j = 0
+
+    // While we still have elements in both arrs... Build result arr
+    while( i < a.length && j < b.length){
+        
+        if( a[i] < b[j] ){
+            result.push(a[i])
+            i++
+        }
+        else{
+            result.push(b[j])
+            j++
+        }
+
+    }
+
+    // If there any any left over elements that haven't been processed in 1st arr...
+    while( i < a.length ){
+        result.push(a[i])
+        i++
+    }
+    
+    // If there any any left over elements that haven't been processed in 2nd arr...
+    while( j < b.length ){
+        result.push(b[j])
+        i++
+    }
+
+    console.log(result)
+
+    return result
+
+}
+
+mergeSort( [1,3,9],[2,4,6,8] )
