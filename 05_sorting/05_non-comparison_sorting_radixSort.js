a1088190d7648d1b83c30ba727f7e325a5718a4d
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-02-19 14:13:11 -0800

    Added radix sort helpers

--- 05_sorting/05_non-comparison_sorting_radixSort.js
+++ 05_sorting/05_non-comparison_sorting_radixSort.js
@@ -7,6 +7,23 @@
 // pass in num and return the digit at a given place (assumes base 10)
 // Example: getDigit(12345,1) --> 4
 // Example: getDigit(12345,2) --> 3
-function getDigit(num, place){
-    return Math.floor(Math.abs(num)/ Math.pow(10,place)) % 10
+function getDigit(num, place, base=10){
+    return Math.floor(Math.abs(num)/ Math.pow(base,place)) % 10
+}
+
+// Get number of digits in a number
+function digitCount(num){
+    if(num === 0) return 1  // protects againts returning -Infinity
+    return matchMedia.floor(Math.log10(Math.abs(num))) + 1
+}
+
+// Accetps array of nums and returns highest number digits that any of those numbers have (uses digitCount)
+// Example: mostDigits([123,2,1234,2]) --> 4
+ function mostDigits(arr){
+    let maxDigits = 0
+    for(let i=0; i<arr.length;i++){
+        maxDigits=Math.max(maxDigits, digitCount(arr[i]))
+    }
+    return maxDigits
+
 }
