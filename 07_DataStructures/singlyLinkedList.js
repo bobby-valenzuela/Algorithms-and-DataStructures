d16e98c12e717d0de824ba67ec3d45022a8a62f8
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-02-25 17:41:13 -0800

    Simple implementation of SinglyLinkedList

---
+++ 07_DataStructures/singlyLinkedList.js
@@ -0,0 +1,40 @@
+class SinglyLinkedList { 
+    constructor() { 
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) { 
+        // Create new node (new tail)
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head;
+        }
+        else { 
+            // Make old tail point to newly created node
+            this.tail.next = newNode;
+            // Make new tail the newNode itself
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+class Node{
+    constructor(val) { 
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const list = new SinglyLinkedList();
+list.push("first");
+list.push("second");
+list.push("third");
+
+// SinglyLinkedList {
+//     head: Node { val: 'first', next: Node { val: 'second', next: [Node] } },
+//     tail: Node { val: 'third', next: null },
+//     length: 3
