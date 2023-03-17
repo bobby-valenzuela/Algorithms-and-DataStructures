81181f5e89d2c42702f9d924b57a2f1e75930a4f
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-03-17 08:17:27 -0700

    Added stack data structure

---
+++ 07_DataStructures/stack.js
@@ -0,0 +1,51 @@
+/* 
+This stack will be linked-list-based (as opposed to an array-based implementation).
+A stack CAN be created using the pop/push method of an array or the shift/unshit methods.
+Pop/push is preferred as using the shift/unshift methods requires to re-calulate all the indexes.
+
+Linked lists are preferred over arrays to make a staack as Linked lists don't carry the extra baggae of indexes that arrays do.
+
+*/
+
+class Stack { 
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) { 
+        const newNode = new Node(val);
+
+        // If this is first node we're adding...
+        if (this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else { 
+            const newSecondNode = this.first;
+            newNode.next = newSecondNode;
+            this.first = newNode;
+        }
+        return ++this.size;
+    }
+
+    pop() { 
+        if (this.size === 0) return null;
+
+        const removedNode = this.first;
+        
+        if (this.size === 1) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return removedNode.val;
+    }
+}
+class Node{ 
+    constructor(val) { 
+        this.val = val;
+        this.next = null;
+    }
+}
