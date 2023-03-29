c6da45c5e3fd70a956b73a56d8bd95de302265b7
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-03-29 15:10:22 -0700

    Added findParent and remove method

--- 07_DataStructures/binarySearchTree.js
+++ 07_DataStructures/binarySearchTree.js
@@ -61,8 +61,10 @@ class BinarySearchTree {
             // If no such node exists - then we failed to find value
             if (!current) return undefined;
 
+            // If we found what we're looking for - break
             if (current.value === value) return foundValue = true && current;
             
+            // Othwerwise, keep traversing tree until we find it or no node exists
             if (value < current.value) {
                 current = current.left;
             }
@@ -74,6 +76,53 @@ class BinarySearchTree {
 
     }
 
+    // Returns parent and child placement
+    findParent(value) { 
+
+        let foundValue = false;
+        let current = this.root; // Starting searching at root
+        let previous = null;    // Save prev node so that if we find current - we know the pre is the parent
+        let placement = null;   // Is node right/left of parent?
+
+        while (!foundValue) { 
+            // If no such node exists - then we failed to find value
+            if (!current) return undefined;
+
+            // If we found what we're looking for - break
+            if (current.value === value) {
+                foundValue = true;
+                break;
+            }
+
+            // Othwerwise, keep traversing tree until we find it or no node exists
+            if (value < current.value) {
+                previous = current;
+                current = current.left;
+                placement = 'left';
+            }
+            else if(value > current.value) { 
+                previous = current;
+                current = current.right;
+                placement = 'right';
+            }
+
+        }
+
+        return [previous,placement];
+
+    }
+
+    // Find node - remove it from parent node, basically cut this branch (or leaf if it's a barren node)
+    remove(value) { 
+        // Save removed node ti return later
+        const foundNode = this.find(value);
+        if (!foundNode) return undefined;
+
+        const [parent, side] = this.findParent(value);
+        parent[side] = null;
+        return foundNode;
+    }
+
 }
 
 class Node { 
@@ -82,6 +131,7 @@ class Node {
         this.left = null;
         this.right = null;
     }
+    // Using left/right for readability - though it would be more verbose to write as leftChild and rightChild
 }
 
 const tree = new BinarySearchTree();
