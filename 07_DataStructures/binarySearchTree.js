6061fdfba0cb4055089a73ca52a2a91dc03bbad2
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-03-28 17:46:20 -0700

    Added insert method

--- 07_DataStructures/binarySearchTree.js
+++ 07_DataStructures/binarySearchTree.js
@@ -3,6 +3,51 @@ class BinarySearchTree {
         this.root = null;
     }
 
+    insert(value) { 
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        else { 
+
+            let current = this.root;
+
+            if (value === current.value) return undefined;
+
+            while (true) { 
+
+                if (value < current.value) { 
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    }
+                    else { 
+                        current = current.left;
+                    }
+                }
+                
+                if (value > current.value) { 
+                    
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    }
+                    else { 
+                        current = current.right;
+                    }
+
+                }
+                
+
+            }
+
+        }
+
+    }
+
 }
 
 class Node { 
@@ -12,3 +57,11 @@ class Node {
         this.right = null;
     }
 }
+
+// const tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.left = new Node(7);
+// tree.right = new Node(15);
+// tree.left.right = new Node(9);
+
+
